80a9ce508d46fed67db7672c6966053388baf368
anton.bilchuk <anton.bilchuk@stylelounge.com>
2017-10-10 01:32:28 +0300

    name refactor, treeInsert func

--- tree.hs
+++ tree.hs
@@ -1,11 +1,13 @@
+-- binary tree
+
 data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Ord, Eq)
 
-search :: (Eq a, Ord a) => a -> Tree a -> Bool
-search _ EmptyTree = False
-search val (Node nodeVal left right) = case val `compare` nodeVal of
+treeElem :: (Eq a, Ord a) => a -> Tree a -> Bool
+treeElem _ EmptyTree = False
+treeElem val (Node nodeVal left right) = case val `compare` nodeVal of
     EQ -> True
-    LT -> search val left 
-    GT -> search val right
+    LT -> treeElem val left 
+    GT -> treeElem val right
 
 tree' = (Node 5 
             (Node 2 
@@ -18,3 +20,22 @@ tree' = (Node 5
                 (Node 12 EmptyTree EmptyTree)
             )
         )
+
+-- treeElem 12 tree'
+
+singelton :: a -> Tree a
+singelton x = Node x EmptyTree EmptyTree
+
+tree'' = Node 5 
+    (Node 2 (singelton 1) (singelton 3))
+    (Node 10 (singelton 8) (singelton 12))
+
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert el EmptyTree = singelton el
+treeInsert el tree@(Node x left right) = case el `compare` x of 
+    EQ -> tree
+    LT -> Node x (treeInsert el left) right
+    GT -> Node x left (treeInsert el right)
+
+-- treeInsert 6 tree''
